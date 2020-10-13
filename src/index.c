0352bc32a8dc02a920d2ac734619a29fa257b639
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-13 11:21:20 +0100

    sqlite #2
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- src/index.c
+++ src/index.c
@@ -4,8 +4,15 @@
 #include <unistd.h>
 #include <math.h>
 #include <time.h>
+#include <sqlite3.h>    // SQLite header (from /usr/include)
 
 
+
+sqlite3 *db;        // database connection
+sqlite3_stmt *stmt;                                                                         /* 1 */
+char *zErrMsg = 0;
+int rc;             // return code
+char *errmsg;       // pointer to an error string
 float cpuUsage, lastCpuUsage;
 float memUsage, lastMemUsage;
 time_t rawtime;
@@ -15,6 +22,29 @@ char* token;
 int i = 0,times,lag;
 long int sum = 0, idle, lastSum = 0,lastIdle = 0;
 long double idleFraction;
+
+static int sqlcallback(void *NotUsed, int argc, char **argv, char **azColName) {
+    int i;
+     return 0;
+}
+
+void sqlconnect(){
+    char *sql = "CREATE TABLE IF NOT EXISTS performance " \
+        "("  \
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, " \
+        "timestamp INTEGER UNIQUE, " \
+        "cputemp INTEGER NOT NULL, " \
+         "cpuusage INTEGER NOT NULL, " \
+        "cpuclock INTEGER NOT NULL" \
+    ");";
+    rc = sqlite3_open("test.db", &db);
+    rc = sqlite3_exec(db, sql, sqlcallback, 0, &zErrMsg);
+    if( rc != SQLITE_OK ){
+        sqlite3_free(zErrMsg);
+    } else {
+    }
+    sqlite3_close(db);
+}
 
 void clearScreen(){
     printf("\033[2J");
@@ -91,6 +121,12 @@ int cpuUsageDetect(){
 
 int main(){
     int round = 0;
+    char buffer[50] = "";
+    char *sql = "INSERT INTO performance " \
+        "(timestamp, cputemp, cpuusage, cpuclock)" \
+        " VALUES " \
+        "(%d, %d, %d, %d);";
+    sqlconnect();
     tableMake();
     while(0 == 0){
         if(round == 30){
@@ -99,10 +135,18 @@ int main(){
         }else{
            round++;
         }
-        tempDetect();
-        freqDetect();
-        cpuUsageDetect();
+        rc = sqlite3_open("test.db", &db);
+        sprintf(
+            buffer, 
+            sql,
+            (int)time(NULL),
+            tempDetect(), 
+            cpuUsageDetect(),
+            freqDetect()
+        );  // faster than the prepare statemanet and that just numbers
         fflush(stdout); 
+        rc = sqlite3_exec(db, buffer, sqlcallback, 0, &zErrMsg);
+        sqlite3_close(db);
         usleep(1000000); 
     }
 }
